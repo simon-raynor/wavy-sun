e5ab57cedc5cef397aafdfe3b641513dd51bdb82
Simon Raynor <ssraynor@googlemail.com>
2026-04-19 20:24:55 +0100

    switched over to canvas rendering for perf

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,205 @@
 import './style.css'
-import createSunflare from './sunflare';
+import tween from './tween';
+
+type Vec2 = [number, number]
 
 
 
-const R = 40;
+const canvas = document.createElement('canvas');
+
+resizeCanvasToScreen(canvas);
+
+document.body.append(canvas);
 
 
-const stage = document.getElementById('stage');
+function resizeCanvasToScreen(canvas: HTMLCanvasElement) {
+    // account for different screen resolutions
+    canvas.height = window.innerHeight * window.devicePixelRatio;
+    canvas.width = window.innerWidth * window.devicePixelRatio;
+    canvas.style.height = window.innerHeight + 'px';
+    canvas.style.width = window.innerWidth + 'px';
+}
 
-const FLARE_ID = 'sunflare'
-
-
-const [symbols, update] = createSunflare(FLARE_ID, R)
-
-stage.querySelector('defs')!.append(symbols);
-
-
-const sun = document.createElementNS('http://www.w3.org/2000/svg', 'use');
-
-sun.setAttribute('href', `#${FLARE_ID}`);
-
-stage.append(sun);
+const ctx = canvas.getContext('2d')!;
+// account for different screen resolutions
+ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
 
 
-let t = 0;
-function loop() {
-    update(t);
 
-    t++;
+function polarToXY([cx, cy]: Vec2, radius: number, angle: number): Vec2 {
+    const theta = Math.PI * (angle / 180);
+    const sin = Math.sin(theta);
+    const cos = Math.cos(theta);
+    const x = cx + radius * cos;
+    const y = cy + radius * sin;
+
+    return [x, y];
+}
+
+
+
+// N.B. final number not used due to how we tween in/out
+const BASE_RADII = [15, 13, 11, 9, 7, 0];
+const SECONDS_PER_WIGGLE = 1;
+const SECONDS_PER_CYCLE = SECONDS_PER_WIGGLE * 2;
+const SECONDS_PER_SPIN = 5;
+
+
+
+function wigglyLine(from: Vec2, to: Vec2, radii: number[], reversed: boolean) {
+    const [fx ,fy] = from;
+    const [tx, ty] = to;
+    const dx = tx - fx;
+    const dy = ty - fy;
+
+    const total = radii.reduce((m, c) => m + c, 0);
+
+    const length = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+    let lineAngle = Math.asin(dy / length);
+    if (dx < 0) lineAngle =  Math.PI - lineAngle;
+
+    
+    ctx.save();
+    ctx.translate(fx, fy);
+    ctx.rotate(lineAngle);
+
+    let cumulativeX = 0;
+
+    radii.forEach((r, i) => {
+        const x = r * length / total;
+
+        const isOdd = !!(i % 2) !== reversed;
+        const startAngle = isOdd
+            ? -5 * Math.PI / 4
+            : -3 * Math.PI / 4;
+        const endAngle = isOdd
+            ? -7 * Math.PI / 4
+            : -1 * Math.PI / 4;
+        const radius = x / Math.SQRT2;
+        const y = isOdd ? -radius / Math.SQRT2 : radius / Math.SQRT2;
+
+        ctx.arc(cumulativeX + (x / 2), y, radius, startAngle, endAngle, isOdd);
+
+        cumulativeX += x;
+    });
+
+    ctx.restore();
+}
+
+function drawLayer(t: number, centre: Vec2, radius: number, flareCount: number, flareLength: number = 1, spin: number = 1) {
+    const wiggleT = (t % SECONDS_PER_WIGGLE) / SECONDS_PER_WIGGLE;
+    const wiggleFlip = (t % SECONDS_PER_CYCLE) >= SECONDS_PER_WIGGLE
+        != spin > 0;
+
+    ctx.beginPath();
+    
+    const degreesPer = 360 / flareCount;
+    const radiusPoints: Vec2[] = [];
+    const spikePoints: Vec2[] = [];
+    const spinOffset = spin * degreesPer * (t % SECONDS_PER_SPIN) / SECONDS_PER_SPIN;
+
+    for (let angle = 0; angle < 360; angle += degreesPer) {
+        radiusPoints.push(polarToXY(centre, radius, angle + spinOffset));
+        spikePoints.push(polarToXY(
+            centre,
+            radius + (radius * flareLength),
+            angle + (degreesPer / 2) + spinOffset
+        ));
+    }
+
+    const wiggleRadii = BASE_RADII.map(
+        (radius, idx) => {
+            if (!idx) {
+                return tween(0, radius, wiggleT);
+            } else if (idx === BASE_RADII.length - 1) {
+                return tween(BASE_RADII[idx - 1], 0, wiggleT);
+            } else return tween(BASE_RADII[idx - 1], radius, wiggleT);
+        }
+    );
+    const wiggleRadRev = wiggleRadii.slice().reverse();
+
+    radiusPoints.forEach((point, idx) => {
+        const spike = spikePoints[idx];
+        const nextpoint = radiusPoints[(idx + 1) % flareCount];
+        
+        wigglyLine(point, spike, wiggleRadii, false !== wiggleFlip);
+        wigglyLine(spike, nextpoint, wiggleRadRev, !!(BASE_RADII.length % 2) !== wiggleFlip);
+    });
+
+
+}
+
+function drawSun(t: number, centre: Vec2, radius: number, layers: number = 2) {
+    const baseFlareLength = 1;
+    const layerRadius = radius / (1 + baseFlareLength);
+
+    const sizeStep = 0.8;
+    const flareStep = 1;
+
+    for (let i = 0; i < layers; i++) {
+        drawLayer(
+            t,
+            centre,
+            layerRadius * Math.pow(sizeStep, i),
+            15 - 2,
+            baseFlareLength / Math.pow(flareStep, i),
+            (i % 2) ? 1 + i : -1 - i
+        );
+        paintCurrentPath();
+    }
+
+    ctx.beginPath();
+    const circRadius = layerRadius * Math.pow(sizeStep, layers);
+    ctx.moveTo(centre[0] + circRadius, centre[1]);
+    ctx.arc(centre[0], centre[1], circRadius, 0, 2 * Math.PI, false);
+    paintCurrentPath();
+}
+
+
+function paintCurrentPath() {
+    const LINE_WIDTH = 4 / window.devicePixelRatio;
+    const FILL = 'gold';
+    const STROKE = 'lightgoldenrodyellow';
+    const SHADOW = 'goldenrod';
+
+    ctx.lineJoin = 'round';
+
+    ctx.strokeStyle = SHADOW;
+    ctx.lineWidth = LINE_WIDTH * 5;
+    ctx.stroke();
+
+    ctx.strokeStyle = FILL;
+    ctx.lineWidth = LINE_WIDTH * 3;
+    ctx.stroke();
+
+    ctx.fillStyle = FILL;
+    ctx.fill();
+
+    ctx.strokeStyle = STROKE;
+    ctx.lineWidth = LINE_WIDTH;
+    ctx.stroke();
+}
+
+
+//let t = 0;
+let prev = 0;
+function loop(t: number) {
+    prev = t;
+
+    ctx.clearRect(0,0,ctx.canvas.width / window.devicePixelRatio,ctx.canvas.height / window.devicePixelRatio);
+    
+    try {
+        drawSun(
+            t / 1000,
+            [window.innerWidth / 2, window.innerHeight / 3],
+            Math.min(window.innerWidth, window.innerHeight) / 3,
+            3
+        );
+    } catch(ex) {
+        console.error(ex);
+    }
+
     requestAnimationFrame(loop);
 }
 
-loop();
+requestAnimationFrame(loop);
